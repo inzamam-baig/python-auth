3455472d3aca415012ab636d724e4690498c99b1
Inzamam Baig <inzamam.baig@broadvisions.net>
2021-06-18 05:10:25 -0700

    Python JWT

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'User {self.username}, password {self.password}'
+
+
+users = [
+    User(1,'test', '123'),
+    User(1,'test', '123')
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+
+jwt = JWT(app, authenticate, identity)
+
+
+@app.route("/")
+@jwt_required()
+def protected():
+    return f'{current_identity}'
+
+
+
+if __name__ == '__main__':
+    app.run()
